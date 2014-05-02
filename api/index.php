03e0eff7b1c9e2b6ee22d134dc4473ee8d42067b
vikekh <vieekk@gmail.com>
2014-05-02 22:01:04 +0200

    Maturing?

--- api/index.php
+++ api/index.php
@@ -1,10 +1,22 @@
 <?php
 
-require '../vendor/autoload.php';
-require '../inc/DbInterface.php';
-require '../inc/DropboxDb.php';
+require 'vendor/autoload.php';
+require 'lib/DbInterface.php';
+require 'lib/DropboxDb.php';
 
+$app = new \Slim\Slim();
 $db = new DropboxDb();
-echo '<pre>';
-var_dump($db->getBeers());
-echo '</pre>';
+
+function respond($response, $status = 200) {
+    $app = \Slim\Slim::getInstance();
+    $app->response->setStatus($status);
+    $app->response->headers->set('Content-Type', 'application/json');
+    echo json_encode($response);
+}
+
+$app->get("/beers", function () use ($app, $db) {
+    $beers = $db->getBeers();
+    respond(200, $beers);
+});
+
+$app->run();
